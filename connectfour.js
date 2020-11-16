ee639e84d4855d4fbbd31d436c5fbbfa6d1126b2
Jose Bolanos <bolanosjjr@gmail.com>
2020-11-16 13:51:00 -0600

    fixed some issues and changed margins

--- connectfour.js
+++ connectfour.js
@@ -1,34 +1,21 @@
-/** Connect Four
- *
- * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
- * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
- */
-
 const WIDTH = 7;
 const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
+
 
 function makeBoard() {
-// TODO: set "board" to empty HEIGHT x WIDTH matrix array
 for(let y = 0; y < HEIGHT; y++){
     board.push(Array.from({length : WIDTH}));
 }
 }
 
-/** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
-const board = document.getElementById('board');
-  // TODO: add comment for this code
-  //creating a top row for the "game piece" to start at and creating an event to listen for a click
+  
+  const board = document.getElementById('board');
+   //creating a top row for the "game piece" to start at and creating an event to listen for a click
   const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
@@ -40,7 +27,6 @@ const board = document.getElementById('board');
   }
   board.append(top);
 
-  // TODO: add comment for this code
   //adding table data for each column, giving an id to each cell based on postioning
   //adding all tds to row
   for (let y = 0; y < HEIGHT; y++) {
@@ -54,10 +40,7 @@ const board = document.getElementById('board');
   }
 }
 
-/** findSpotForCol: given column x, return top empty y (null if filled) */
-
-function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
+function findSpotForCol(x) {  
 for(let y = HEIGHT - 1; y >= 0; y--){
     if(!board[y][x]){
         return y;
@@ -66,33 +49,26 @@ for(let y = HEIGHT - 1; y >= 0; y--){
   return null;
 }
 
-/** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
  const piece = document.createElement('div');
  piece.classList.add('piece');
  piece.classList.add(`p${currPlayer}`);
-//  if(currPlayer === 1){
-//      addPiece.classList.add('p1')
-//  }else {
-//      addPiece.classList.add('p2')
-//  }
  const getCircle = document.getElementById(`${y}-${x}`);
  getCircle.append(piece);
 }
 
-/** endGame: announce game end */
+
 
 function endGame(msg) {
-  // TODO: pop up alert message
-  alert(msg);
+
+  setTimeout(function(){
+    alert(msg);
+    },300);
 }  
 
-/** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
-  // get x from ID of clicked cell
+
   const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
@@ -102,7 +78,6 @@ function handleClick(evt) {
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
   board[y][x] = currPlayer;
   placeInTable(y, x);
   
@@ -113,13 +88,11 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
   if(board.every(row => row.every(cell => cell))) {
     return endGame('Tie Game! Try Again');
   }
 
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
   currPlayer === 1 ? (currPlayer = 2) : (currPlayer = 1);
 }
 
@@ -128,14 +101,10 @@ const resetButton = document.getElementById('reset-button');
 resetButton.addEventListener('click',function(){
     document.location.href = "";
 });
-/** checkForWin: check board cell-by-cell for "does a win start here?" */
 
 function checkForWin() {
   function _win(cells) {
-    // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
-    //  - returns true if all are legal coordinates & all match currPlayer
-
+   
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
@@ -146,7 +115,6 @@ function checkForWin() {
     );
   }
 
-  // TODO: read and understand this code. Add comments to help you.
 //iterating over the entire game board and looks for 4 of the same color in any direction if any of the arrays contain a direction then returning true to declare winner
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
